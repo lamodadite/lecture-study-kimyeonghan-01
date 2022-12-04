97d3095416307fb697a00954f7bcf5244bdfb2e3
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-04 21:00:40 +0900

    Initial commit

---
+++ src/test/java/com/example/lecturestudykimyeonghan01/LectureStudyKimyeonghan01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecturestudykimyeonghan01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureStudyKimyeonghan01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
